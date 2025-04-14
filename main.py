c47316faa44664d16e200782b35f3bb30b0f149e
Oluwayomi Felix <77457879+Portia-dot@users.noreply.github.com>
2025-04-14 08:08:31 -0700

    Update

--- main.py
+++ main.py
@@ -28,29 +28,28 @@ try:
                 data_manager.update_data(sheet_data, column_name = 'IATA Code')
             except Exception as e:
                 print(e)
+        else:
+            iataCode = row['IATA Code']
+            maxPrice = row['Lowest Price']
+            time.sleep(10)
+            try:
+                result = flight_search.get_flight_offer(destination_location_code=iataCode, max_price=maxPrice)
+                if not result:
+                    print('No flight offer found')
+                    continue
+                fromCity = result['origin']
+                toCity = result['destination']
+                departureDate = result['departureDate']
+                returnDate = result['returnDate']
+                price = result['price']
 
-#         else:
-#             iataCode = row['IATA Code']
-#             maxPrice = row['Lowest Price']
-#             time.sleep(10)
-#             try:
-#                 result = flight_search.get_flight_offer(destination_location_code=iataCode, max_price=maxPrice)
-#                 if not result:
-#                     print('No flight offer found')
-#                     continue
-#                 fromCity = result['origin']
-#                 toCity = result['destination']
-#                 departureDate = result['departureDate']
-#                 returnDate = result['returnDate']
-#                 price = result['price']
-#
-#                 message = f'{price['total']} Pounds {fromCity} to {toCity}! on {departureDate} to {returnDate}'
-#                 all_messages.append(message)
-#             except requests.exceptions.RequestException as e:
-#                 print('Handled Expected exception',e)
-#     # if all_messages:
-#     #     full_message = '\n\n'.join(all_messages)
-#     #     message_manager.send(full_message)
+                message = f'{price['total']} Pounds {fromCity} to {toCity}! on {departureDate} to {returnDate}'
+                all_messages.append(message)
+            except requests.exceptions.RequestException as e:
+                print('Handled Expected exception',e)
+    # if all_messages:
+    #     full_message = '\n\n'.join(all_messages)
+    #     message_manager.send(full_message)
 except Exception as e:
     print(e)
 
