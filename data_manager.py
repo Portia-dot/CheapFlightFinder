f955b7d34451fc3e0ca621fef3c996305b7fbb16
Oluwayomi Felix <77457879+Portia-dot@users.noreply.github.com>
2025-04-14 08:06:13 -0700

    Changed from Sheety API to use Google API due to Sheety API limitations

--- data_manager.py
+++ data_manager.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 from dotenv import load_dotenv
 import requests
@@ -18,18 +19,17 @@ class DataManager:
         self.sheet_id = google_sheet_id
         self.sheet = self.client.open_by_key(self.sheet_id)
 
-        values_list = self.sheet.sheet1.row_values(1)
-        print(values_list)
-
-
-        self.file = 'Flights - prices.csv'
-        self.data = self.get_data()
-
     #Get Request
     def get_data(self):
-        return pd.read_csv(self.file)
+        values_list = self.sheet.sheet1.get_values()
+        headers = values_list[0]
+        rows = values_list[1:]
+        df = pd.DataFrame(rows, columns=headers)
+        return df
     #  Put Request
-    def update_data(self, index, new_data):
-        df = pd.read_csv(self.file)
-        df.loc[index, 'IATA Code'] = new_data['IATA Code']
-        df.to_csv(self.file, index=False)
+    def update_data(self,dataframe, column_name):
+        dataframe[column_name] = dataframe[column_name]
+        value_to_write = [dataframe.columns.tolist()] + dataframe.values.tolist()
+        worksheet = self.sheet.sheet1
+        worksheet.clear()
+        worksheet.update(values=value_to_write)
--- main.py
+++ main.py
@@ -19,42 +19,38 @@ sheet_data = data_manager.get_data()
 try:
     for index, row in sheet_data.iterrows():
         if row['IATA Code'] == '':
-            Id = index
-            city = row['city']
+            city = row['City']
             time.sleep(5)
             try:
                 response = flight_search.get_city_iata(city)
                 iataCode = response['data'][0]['iataCode']
-                new_data = {
-                    'price':{
-                        'iataCode':iataCode,
-                    }
-                }
-                data = data_manager.update_data(Id, new_data)
+                sheet_data.at[index, 'IATA Code'] = iataCode
+                data_manager.update_data(sheet_data, column_name = 'IATA Code')
             except Exception as e:
                 print(e)
-        else:
-            iataCode = row['IATA Code']
-            maxPrice = row['Lowest Price']
-            time.sleep(10)
-            try:
-                result = flight_search.get_flight_offer(destination_location_code=iataCode, max_price=maxPrice)
-                if not result:
-                    print('No flight offer found')
-                    continue
-                fromCity = result['origin']
-                toCity = result['destination']
-                departureDate = result['departureDate']
-                returnDate = result['returnDate']
-                price = result['price']
 
-                message = f'{price['total']} Pounds {fromCity} to {toCity}! on {departureDate} to {returnDate}'
-                all_messages.append(message)
-            except requests.exceptions.RequestException as e:
-                print('Handled Expected exception',e)
-    # if all_messages:
-    #     full_message = '\n\n'.join(all_messages)
-    #     message_manager.send(full_message)
+#         else:
+#             iataCode = row['IATA Code']
+#             maxPrice = row['Lowest Price']
+#             time.sleep(10)
+#             try:
+#                 result = flight_search.get_flight_offer(destination_location_code=iataCode, max_price=maxPrice)
+#                 if not result:
+#                     print('No flight offer found')
+#                     continue
+#                 fromCity = result['origin']
+#                 toCity = result['destination']
+#                 departureDate = result['departureDate']
+#                 returnDate = result['returnDate']
+#                 price = result['price']
+#
+#                 message = f'{price['total']} Pounds {fromCity} to {toCity}! on {departureDate} to {returnDate}'
+#                 all_messages.append(message)
+#             except requests.exceptions.RequestException as e:
+#                 print('Handled Expected exception',e)
+#     # if all_messages:
+#     #     full_message = '\n\n'.join(all_messages)
+#     #     message_manager.send(full_message)
 except Exception as e:
     print(e)
 
